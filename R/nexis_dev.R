8e893146f49aa29fb9f04ee6c2a2d9cead343b2d
Kohei Watanabe <watanabe.kohei@gmail.com>
2018-12-20 14:35:30 +0900

    Prototype for Nexis Advance

--- R/nexis_dev.R
+++ R/nexis_dev.R
@@ -1,59 +1,67 @@
 require(XML)
 require(stringi)
+require(xml2)
 
-import_doc <- function(file, paragraph_separator) {
 
+
+#out <- antiword::antiword("tests/data/nexis/nyt.docx")
+#out <- docx_extract_all_tbls(read_docx("tests/data/nexis/nyt.docx"))
+
+#import_doc <- function(file, paragraph_separator) {
+    #file <- "tests/data/nexis/nyt.docx"
+    file <- "tests/data/nexis/usa-today.docx"
+    #file <- "tests/data/nexis/washington-post.docx"
     cat('Reading', file, '\n')
 
     unzip(file, "word/document.xml", exdir = tempdir())
     file <- paste0(tempdir(), "/word/document.xml")
-    html <- paste0(readLines(file, warn = FALSE, encoding = "UTF-8"), collapse = "")
-
-    # # fix tags
-    # html <- stri_replace_all_fixed(html, "&nbsp;<br><pre></pre>", "")
-    # html <- stri_replace_all_fixed(html, "<li>", " ")
-    # html <- stri_replace_all_fixed(html, "</li>", " ")
-    # html <- stri_replace_all_fixed(html, "<p><p>", "</p><p>")
-    #
-    # # separate articles
-    # html <- stri_replace_all_fixed(html, "<h1>", "<doc><h1>")
-    # html <- stri_replace_all_fixed(html, "<p></p><br><p></p><br><p></p>", "</doc>")
-
-    dom <- htmlParse(html, encoding = "UTF-8")
-
-    length(getNodeSet(dom, "//*[name()='w:titlePg']"))
-    getNodeSet(dom, '//w')
-
+    xml <- paste0(readLines(file, warn = FALSE, encoding = "UTF-8"), collapse = "")
+    dom <- xmlParse(xml, encoding = "UTF-8")
+    elems <- getNodeSet(dom, "//w:p[./w:hyperlink/w:r/w:rPr/w:sz[@w:val='28'] and
+                                   ./w:hyperlink/w:r/w:rPr/w:color[@w:val='0077CC']]")
+    n <- length(elems)
     data <- data.frame()
-    for(doc in getNodeSet(dom, '//w:titlePg')){
-
+    for (i in seq(n)) {
+        if (i + 1 > n) break
         attrs <- list(pub = "", date = "", head = "", body = "")
-
-        # h1s <- getNodeSet(doc, './h1')
-        # if (length(h1s) == 1)
-        #     attrs$head <- stri_trim_both(xmlValue(h1s[[1]]))
-        #
-        # h2s <- getNodeSet(doc, './h2')
-        # if (length(h2s) == 2) {
-        #     attrs$pub <- stri_trim_both(xmlValue(h2s[[1]]))
-        #     time <- unlist(stri_split_fixed(stri_trim_both(xmlValue(h2s[[2]])), "."))
-        #     attrs$date <- paste(rev(time), collapse = "-")
-        # }
-        #
-        # ps <- getNodeSet(doc, './p')
-        # if (length(ps) > 0) {
-        #     p <- sapply(ps, xmlValue)
-        #     attrs$body <- stri_trim(paste0(p, collapse = paste0(' ', paragraph_separator, ' ')))
-        # }
-        #
-        # if (attrs$pub[1] == '' || is.na(attrs$pub[1])) warning('Failed to extract publication name')
-        # if (attrs$date[1] == '' || is.na(attrs$date[1])) warning('Failed to extract date')
-        # if (attrs$head[1] == '' || is.na(attrs$head[1])) warning('Failed to extract heading')
-        # if (attrs$body[1] == '' || is.na(attrs$body[1])) warning('Failed to extract body text')
-        #
-        # data <- rbind(data, as.data.frame(attrs, stringsAsFactors = FALSE))
+        elem <- elems[[i]]
+        body <- character()
+        is_body <- FALSE
+        j <- 1
+        while (TRUE) {
+            str <- stri_trim(xmlValue(elem))
+            if (stri_detect_regex(str, "^\\(.{0,100}\\)$")) {
+                elem <- getSibling(elem, after = TRUE)
+                next
+            }
+            if (stri_detect_regex(str, "^Load-Date:\\s")) {
+                break
+            }
+            if (j == 1)
+                attrs$head <- str
+            if (j == 2)
+                attrs$pub <- str
+            if (j == 3)
+                attrs$date <- str
+            #if (str == "End of Document") break
+            if (stri_detect_regex(str, "^Section:\\s"))
+                attrs$section <- stri_trim(stri_replace_first_fixed(str, "Section: ", ""))
+            if (stri_detect_regex(str, "^Length:\\s"))
+                attrs$length <- stri_trim(stri_replace_first_regex(str, "Length:\\s(\\d+) words", "$1"))
+            if (is_body)
+                body <- c(body, str)
+            if (str == "Body") is_body <- TRUE
+            elem <- getSibling(elem, after = TRUE)
+            if (identical(elem, elems[[i + 1]])) break
+            if (is.null(elem)) break
+            j <- j + 1
+        }
+        body <- body[nzchar(body)]
+        attrs$body <- paste(body, collapse = " ")
+        data <- rbind(data, as.data.frame(attrs, stringsAsFactors = FALSE))
     }
-    return(data)
-}
 
-import_doc("tests/data/nexis/nyt_1-50.docx")
+#    return(data)
+#}
+
+#import_doc("tests/data/nexis/nyt_1-50.docx")
